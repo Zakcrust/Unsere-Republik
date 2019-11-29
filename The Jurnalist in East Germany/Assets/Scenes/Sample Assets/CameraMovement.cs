847aadfb9a56b9ac6256c9c21af73762de2308f4
Akbar <akbarhida@gmail.com>
2019-11-29 22:55:40 +0700

    first commit

---
+++ The Jurnalist in East Germany/Assets/Scenes/Sample Assets/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+
+    void Update () {
+        Vector3 offset = transform.position - player.transform.position;
+        bool changeInX = false;
+        Vector3 newCameraPosition = transform.position;
+        if (offset.x > 6)
+        {
+            changeInX = true;
+            newCameraPosition.x = (player.transform.position.x + offset.x) - (offset.x - 6);
+        }
+        else if (offset.x < -6)
+        {
+            changeInX = true;
+            newCameraPosition.x = (player.transform.position.x + offset.x) - (offset.x + 6);
+        }
+        if (!changeInX)
+        {
+            newCameraPosition.x = transform.position.x;
+        }
+        transform.position = newCameraPosition;
+ }
+}
---
+++ The Jurnalist in East Germany/Assets/Scenes/Sample Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rigidBody2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2d = GetComponent <Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        platformMovement();
+    }
+
+    private void platformMovement()
+    {
+        Vector2 currentPosition = transform.position;
+        Vector2 resetSpeed = new Vector2(0,currentPosition.y);
+        rigidBody2d.velocity = resetSpeed;
+        Vector2 velocity = rigidBody2d.velocity;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            velocity.x += speed;
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            velocity.x -= speed;
+        }
+
+        else
+        {
+            velocity.x = 0.0f;
+        }
+
+
+        rigidBody2d.velocity += velocity;
+    }
+
+}
