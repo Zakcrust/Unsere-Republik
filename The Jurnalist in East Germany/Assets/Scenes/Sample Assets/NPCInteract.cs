3584cfc8e1bb7104bcae7b32ded2f8d90cfb84c8
zakcrust <akbarhida@gmail.com>
2019-11-30 01:25:27 +0700

    Pop up ^ Interaction Added

---
+++ The Jurnalist in East Germany/Assets/Scenes/Sample Assets/NPCInteract.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInteract : MonoBehaviour
+{
+    private string NPCText = "This is an NPC text";
+    public GameObject popUp;
+    public GameObject interactionLayout;
+
+    void Start()
+    {
+        popUp.SetActive(false);
+        interactionLayout.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        popUp.SetActive(true);
+        interactionLayout.SetActive(true);
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        popUp.SetActive(false);
+        interactionLayout.SetActive(false);
+    }
+
+}
