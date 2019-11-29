8f0c5b14f9f18ee644117f482dd894af1bc40409
zakcrust <akbarhida@gmail.com>
2019-11-30 00:11:29 +0700

    Movement and Camera Implemented (not Smooth yet)

--- The Jurnalist in East Germany/Assets/Scenes/Sample Assets/PlayerMovement.cs
+++ The Jurnalist in East Germany/Assets/Scenes/Sample Assets/PlayerMovement.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
-
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     Rigidbody2D rigidBody2d;
+    Vector3 playerScale;
+    Animator animator;
+    [SerializeField]
+    private SpriteRenderer playerSprite;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody2d = GetComponent <Rigidbody2D>();
+        animator = GetComponent <Animator>();
+        Vector3 playerScale = transform.localScale;
     }
 
     // Update is called once per frame
@@ -27,20 +32,26 @@ public class PlayerMovement : MonoBehaviour
         Vector2 resetSpeed = new Vector2(0,currentPosition.y);
         rigidBody2d.velocity = resetSpeed;
         Vector2 velocity = rigidBody2d.velocity;
+        
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
             velocity.x += speed;
+            animator.SetBool("isMoving",true);
+            playerSprite.flipX = false;
         }
 
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             velocity.x -= speed;
+            animator.SetBool("isMoving",true);
+            playerSprite.flipX = true;
         }
 
         else
         {
             velocity.x = 0.0f;
+            animator.SetBool("isMoving",false);
         }
 
 
